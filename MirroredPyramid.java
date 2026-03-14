bab74a2ec8b59d728f8f68ebf4e1159458d495c3
vishjapkar <111555732+vishjapkar@users.noreply.github.com>
2026-03-14 15:47:43 +0530

    Add files via upload
    Add mirrored number pyramid pattern program

---
+++ MirroredPyramid.java
@@ -0,0 +1,17 @@
+ class MirroredPyramid {
+    public static void main(String[] args) {
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int s = rows - i; s >= 1; s--) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
